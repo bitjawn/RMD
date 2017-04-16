cb70504c0018d630edc3da343e299fdb49427076
bitjawn <ricwalker59@gmail.com>
2017-04-16 13:35:03 -0400

    Modified the admin's profile view

--- routes/admins.js
+++ routes/admins.js
@@ -4,10 +4,86 @@ var csrf = require('csurf');
 var passport = require('passport');
 var csrfProtection = csrf();
 var cfc = require('../modules/cfc');
+var NodeCouchDb = require('node-couchdb');
+
+const couch = new NodeCouchDb({
+    auth: {
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD
+    }
+});
+const dbName = "menu";
+const viewUrl = '_design/full_menu/_view/all_dishes';
+const sortedTitleViewUrl = '_design/sorted_full_menu/_view/title_sorted';
+const sortedCategoryViewUrl = '_design/sorted_full_menu/_view/category_sorted';
+const queryBreakfast = '_design/queryCategory/_view/all_breakfast';
+const queryLunch = '_design/queryCategory/_view/all_lunch';
+const queryDinner = '_design/queryCategory/_view/all_dinner';
 
 router.get('/profile', isLoggedIn, csrfProtection, function(req, res, next){       
     const greet = 'Greetings ' + cfc(req.user.fname);
-    res.render('admin/profile', {pageTitle:'Admin', greet:greet});
+    // res.render('admin/profile', {pageTitle:'Admin', greet:greet});
+
+    couch.get(dbName, sortedCategoryViewUrl).then(
+        function(data, headers, status) {  
+            res.render('admin/profile', {
+                menu:data.data.rows,
+                pageTitle:cfc('Admin'),
+                greet:greet
+            });            
+        },
+        function(err){
+            res.send(err);
+    });
+});
+
+// search view
+router.get('/search/:category', function(req, res) {
+    var category = req.params.category;
+
+    switch (category.toLowerCase()) {
+        case 'breakfast':
+            couch.get(dbName, queryBreakfast).then(
+                function(data, headers, status) { 
+                    res.render('search', {
+                        menu:data.data.rows,
+                        category:cfc(category),
+                        pageTitle:cfc('Menu')
+                    });            
+                },
+                function(err){
+                    res.send(err);
+            });
+            break;
+
+        case 'lunch':
+            couch.get(dbName, queryLunch).then(
+                function(data, headers, status) {  
+                    res.render('search', {
+                        menu:data.data.rows,
+                        category:cfc(category),
+                        pageTitle:cfc('Menu')
+                    });            
+                },
+                function(err){
+                    res.send(err);
+            });
+            break;
+
+        case 'dinner':
+            couch.get(dbName, queryDinner).then(
+                function(data, headers, status) {  
+                    res.render('search', {
+                        menu:data.data.rows,
+                        category:cfc(category),
+                        pageTitle:cfc('Menu')
+                    });            
+                },
+                function(err){
+                    res.send(err);
+            });
+            break;
+    }
 });
 
 router.get('/logout', csrfProtection, function(req, res, next){
